e8e537074b097f829a8ab5fbcdaa8aa81a982429
C. Titus Brown <titus@idyll.org>
2015-11-18 21:17:32 -0800

    initial foo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Hi, amarie!
+# Project Jupyter workshop materials -- 11/18/15
+
+See
+
+http://dib-training.readthedocs.org/en/pub/2015-11-19-ipython-jupyter.html
---
+++ install-ubuntu.sh
@@ -0,0 +1,49 @@
+# ubuntu-wily-15.10-amd64-server-20151116.1 (ami-26d5af4c)
+
+sudo apt-get -y update
+sudo apt-get -y install r-base python3-matplotlib libzmq3-dev python3.5-dev texlive-latex-extra texlive-latex-recommended python3-virtualenv
+
+cd ~/
+python3 -m virtualenv -p python3.5 env --system-site-packages
+. ~/env/bin/activate
+
+
+jupyter notebook --generate-config
+
+cat >>/home/ubuntu/.jupyter/jupyter_notebook_config.py <<EOF
+c = get_config()
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'sha1:5d813e5d59a7:b4e430cf6dbd1aad04838c6e9cf684f4d76e245c' #davis
+c.NotebookApp.port = 8000
+
+EOF
+
+# install bash kernel, following: https://github.com/takluyver/bash_kernel
+pip install bash_kernel
+python -m bash_kernel.install
+
+
+# install R kernel, following: http://irkernel.github.io/installation/
+cat >>install-irkernel.sh <<EOF
+install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
+                 repos = c('http://irkernel.github.io/',
+                           'http://cran.us.r-project.org'))
+EOF
+
+sudo R < install-irkernel.sh
+echo 'IRkernel::installspec()' | R --no-save
+
+cd
+git clone https://github.com/damianavila/RISE.git
+cd RISE
+python setup.py install
+
+cat >run-notebook.sh <<EOF
+#! /bin/bash
+. ~ubuntu/env/bin/activate
+jupyter notebook
+EOF
+
+chmod +x run-notebook.sh
+screen -D -m run-notebook.sh
